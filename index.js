ef5cdaaa033ccde7990cb37145ec8df1fd5e9bea
Anna Sedlackova <axs1202@case.edu>
2017-11-03 22:43:23 -0400

    Update index.js
    added socket.io

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//variable decleration
+//variable declaration
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(_dirname + '/index.html');
