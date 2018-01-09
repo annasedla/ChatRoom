5143bfcdfbcfeac0478448664162c358a209e4a7
Anna Sedlackova <axs1202@case.edu>
2018-01-09 15:56:41 -0500

    Updated io.on function

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.get('/', function(req, res){
 //connection to socket.io
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 //port to listen on
